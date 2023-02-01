c7a444e2bcb3bd34a363a74c2112730e37526449
Lenovo <Lenovo@SHREYA>
2023-02-01 13:57:19 +0530

    First commit

---
+++ Dayone/src/org/tnsif/intro/SimpleJavaProgramDemo.java
@@ -0,0 +1,11 @@
+package org.tnsif.intro;
+
+public class SimpleJavaProgramDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to java fullstack program");
+		
+		
+	}
+
+}
