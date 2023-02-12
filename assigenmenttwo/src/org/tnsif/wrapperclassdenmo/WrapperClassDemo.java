65c69f9f333ca24376c61bf677cd229631ba787f
Lenovo <Lenovo@SHREYA>
2023-02-12 22:24:51 +0530

    Eight Commit

---
+++ assigenmenttwo/src/org/tnsif/wrapperclassdenmo/WrapperClassDemo.java
@@ -0,0 +1,21 @@
+package org.tnsif.wrapperclassdenmo;
+
+//pogra,m to demonstate on auto and unboxing
+public class WrapperClassDemo {
+
+	public static void main(String[] args) {
+		// autoboxing
+		int x = 18;
+		Integer y = x;
+		System.out.println(y);
+		
+		//unboxing
+		Integer i = new Integer(10);
+		System.out.println(i);
+		int b = i.intValue();
+		System.out.println(b);
+		
+
+	}
+
+}
